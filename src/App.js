9b18efde3f33817124ba1f185a456941794839a4
Dustin <dustinibrao@gmail.com>
2022-01-14 12:02:17 -1000

    delete task function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "Doctors Appointment",
+			day: "Feb 5th at 2:30pm",
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: "meeting",
+			day: "Feb 6th at 2:30pm",
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: "park",
+			day: "Feb 7th at 2:30pm",
+			reminder: true,
+		},
+	]);
+
+	// Delete Task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	};
+
+	return (
+		<div className="container">
+			<Header />
+			<Tasks tasks={tasks} onDelete={deleteTask} />
+		</div>
+	);
 }
 
 export default App;
